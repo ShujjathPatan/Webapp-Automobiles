e5d1c340537616147bfca9af54ef7f848d37af83
StrangeRZUb <113985666+StrangeRZUb@users.noreply.github.com>
2024-10-21 16:56:23 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      sxl: '1536px',
+      xxl: '1537px',
+      servicesxl: '1649px',
+      txl : '1800px',
+      
+      
+    }
+  }
+}
